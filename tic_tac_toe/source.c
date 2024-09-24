bedaa3d4de8aa78f89c3551e4a1cb833f3dff65f
karick4398 <skkqd1983@gmail.com>
2024-09-24 16:48:29 +0900

    틱택토 2일차
    컴퓨터가 가로, 세로, 대각선 순으로 검사하여 배치.
    승리 기능도 만들었다
    
    [문제점]
    gameEnd변수를 만들어 종료 후 Enter를 누르면 메인으로 돌아가게 만들었지만 어째서인지 반응이 없다

--- tic_tac_toe/source.c
+++ tic_tac_toe/source.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdlib.h>
 #include <conio.h> // 콘솔 입출력 함수를 제공
 #include <Windows.h> // 윈도우 용의 수 많은 함수를 제공
 
@@ -32,6 +33,7 @@ int player = 0; // 현재 플레이어를 저장하는 변수
 int gameFlag = 0; // 게임 시작 여부 관련 변수
 int gameMenu = 0; // 선택된 메뉴 관련 변수
 int turnCount = 0; // 게임 턴 횟수 변수
+int gameEnd = 0; //게임 종료 여부 변수
 int XY[2] = { 2, 1 };
 
 Score score = { 0, 0 }; // 게임 스코어 관련 변수
@@ -48,6 +50,8 @@ void gamePlayingScreen();
 void inputGame();
 void map_reset();
 void selectedMap();
+void computer_selectedMap();
+int win_test();
 
 char original_map[50][50] = {
 	{"-------------\n"},
@@ -94,6 +98,12 @@ void gamePlayingScreen() {
 		printf(original_map[i]);
 	}
 
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			ox_map[i][j] = 0;
+		}
+	}
+
 	gotoMouseXY(XY[0], XY[1]);
 	printf("■");
 	while (gameFlag = 1) {
@@ -102,6 +112,7 @@ void gamePlayingScreen() {
 }
 
 void printGameStartScreen() {
+	system("cls");
 	printf("[현재 스코어]\n");
 	printf("Player : %d vs Computer : %d\n\n", score.player, score.computer);
 
@@ -150,42 +161,238 @@ void selectedGameMenu() {
 void inputGame() {
 	char key = getch(); // getch()는 키보드의 하나 키를 입력 받을 수 있게 하는 함수
 
-	if (XY[0] != 2 && key == 'a' || key == 'A') {
+	if (XY[0] != 2 && gameEnd != 1 && (key == 'a' || key == 'A')) {
 		map_reset();
 		XY[0] -= 4;
 		gotoMouseXY(XY[0], XY[1]);
 		printf("■");
 	}
-	else if (XY[0] != 10 && key == 'd' || key == 'D') {
+	else if (XY[0] != 10 && gameEnd != 1 && (key == 'd' || key == 'D')) {
 		map_reset();
 		XY[0] += 4;
 		gotoMouseXY(XY[0], XY[1]);
 		printf("■");
 	}
-	else if (XY[1] != 5 && key == 's' || key == 'S') {
+	else if (XY[1] != 5 && gameEnd != 1 && (key == 's' || key == 'S')) {
 		map_reset();
 		XY[1] += 2;
 		gotoMouseXY(XY[0], XY[1]);
 		printf("■");
 	}
-	else if (XY[1] != 1 && key == 'w' || key == 'W') {
+	else if (XY[1] != 1 && gameEnd != 1 && (key == 'w' || key == 'W')) {
 		map_reset();
 		XY[1] -= 2;
 		gotoMouseXY(XY[0], XY[1]);
 		printf("■");
 	}
 	else if (key == '\r') {
-		selectedMap();
+		if (gameEnd == 0) {
+			selectedMap();
+		}
+		else {
+			gameFlag = 0;
+		}
 	}
 
 	gotoMouseXY(0, 7);
-	printf("%d %d", XY[0], XY[1]);
+	//printf("%d %d", XY[0], XY[1]);
+	for (int i = 0; i < 3; i++) {
+		printf("%d %d %d\n", ox_map[i][0], ox_map[i][1], ox_map[i][2]);
+	}
 }
 
 void selectedMap() {
 	ox_map[(XY[1] + 1) / 2 - 1][(XY[0] + 2) / 4 - 1] = 1;
+	if (win_test(1) == 1) {
+		gotoMouseXY(0, 13);
+		printf("[player] 승리!");
+		gameEnd = 1;
+	}
+	else {
+		computer_selectedMap();
+		map_reset();
+
+		if (win_test(2) == 1) {
+			gotoMouseXY(0, 13);
+			printf("[computer] 승리!");
+			gameEnd = 1;
+		}
+	}
 }
 
+void computer_selectedMap() {
+	srand((unsigned int)time(NULL));
+	int count = 0;
+	
+	for (int i = 0; i < 3; i++) { //검사1 - 가로 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (ox_map[i][j] == 1) {
+				count += 1;
+			}
+			if (count >= 2) { //한 줄에서 1이 2개 발견될 시
+				for (int l = 0; l < 3; l++) { //빈자리 체크 후 2입력
+					if (ox_map[i][l] == 0) {
+						ox_map[i][l] = 2;
+						/*
+						gotoMouseXY(0, 11);
+						printf("가로행 체크");
+						*/
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 3; i++) { //검사2 - 세로 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (ox_map[j][i] == 1) {
+				count += 1;
+			}
+			if (count >= 2) { //한 줄에서 1이 2개 발견될 시
+				for (int l = 0; l < 3; l++) { //빈자리 체크 후 2입력
+					if (ox_map[l][i] == 0) {
+						ox_map[l][i] = 2;
+						/*
+						gotoMouseXY(0, 11);
+						printf("세로행 체크");
+						*/
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 2; i++) { //검사3 - 대각선 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (i == 0) {
+				if (ox_map[j][j] == 1) {
+					count += 1;
+				}
+				if (count >= 2) { //한 줄에서 1이 2개 발견될 시
+					for (int l = 0; l < 3; l++) { //빈자리 체크 후 2입력
+						if (ox_map[l][l] == 0) {
+							ox_map[l][l] = 2;
+							/*
+							gotoMouseXY(0, 11);
+							printf("대각선행1 체크");
+							*/
+							return;
+						}
+					}
+				}
+			}
+			else {
+				if (ox_map[j][2-j] == 1) {
+					count += 1;
+				}
+				if (count >= 2) { //한 줄에서 1이 2개 발견될 시
+					for (int l = 0; l < 3; l++) { //빈자리 체크 후 2입력
+						if (ox_map[l][2-l] == 0) {
+							ox_map[l][2-l] = 2;
+							/*
+							gotoMouseXY(0, 11);
+							printf("대각선행2 체크");
+							*/
+							return;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	while (1) { //검사에서 아무것도 걸리지 않는다면 랜덤 배치
+		int rand1 = (rand() % 3+1)*2;
+		int rand2 = (rand() % 3+1)*4;
+		if (ox_map[rand1 / 2 - 1][rand2 / 4 - 1] == 0) {
+			ox_map[rand1 / 2 - 1][rand2 / 4 - 1] = 2;
+			gotoMouseXY(0, 11);
+			printf("랜덤 지정");
+			break;
+		}
+	}
+}
+
+int win_test(int who) { //각 턴이 끝나면 1 혹은 2를 입력받아 승리했는지 검사
+	
+	int count = 0;
+
+	for (int i = 0; i < 3; i++) { //검사1 - 가로 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (ox_map[i][j] == who) {
+				count += 1;
+			}
+			if (count == 3) { //한 줄에서 1이 3개 발견될 시
+				if (who == 1) {
+					score.player += 1;
+				}
+				else if (who == 2) {
+					score.computer += 1;
+				}
+				return 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < 3; i++) { //검사2 - 세로 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (ox_map[j][i] == who) {
+				count += 1;
+			}
+			if (count == 3) { //한 줄에서 1이 3개 발견될 시
+				if (who == 1) {
+					score.player += 1;
+				}
+				else if (who == 2) {
+					score.computer += 1;
+				}
+				return 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < 2; i++) { //검사3 - 대각선 행 체크
+		count = 0;
+		for (int j = 0; j < 3; j++) {
+			if (i == 0) {
+				if (ox_map[j][j] == who) {
+					count += 1;
+				}
+				if (count == 3) { //한 줄에서 1이 3개 발견될 시
+					if (who == 1) {
+						score.player += 1;
+					}
+					else if (who == 2) {
+						score.computer += 1;
+					}
+					return 1;
+				}
+			}
+			else {
+				if (ox_map[j][2 - j] == who) {
+					count += 1;
+				}
+				if (count == 3) { //한 줄에서 1이 3개 발견될 시
+					if (who == 1) {
+						score.player += 1;
+					}
+					else if (who == 2) {
+						score.computer += 1;
+					}
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
 
 void map_reset() {
 	system("cls");
@@ -197,8 +404,15 @@ void map_reset() {
 		for (int j = 0; j < 3; j++) {
 			if (ox_map[j][i] == 1) {
 				gotoMouseXY(2+(4*i), 1+(2*j));
-				printf("◎");
+				SetConsoleTextAttribute(COUT, BLUE);
+				printf("●");
+			}
+			else if (ox_map[j][i] == 2) {
+				gotoMouseXY(2 + (4 * i), 1 + (2 * j));
+				SetConsoleTextAttribute(COUT, RED);
+				printf("●");
 			}
 		}
 	}
+	SetConsoleTextAttribute(COUT, WHITE);
 }
